981aed6ce8ad18ff150fb79137b836a83e75c01e
Wojciech Zalewski <zalewski.media@gmail.com>
2023-10-27 21:58:38 +0200

    Error middleware added

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
 require("dotenv").config()
 const notFound = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
 
 app.use(express.static("/public"))
 app.use(express.json())
 
 app.use("/api/v1/tasks", tasks)
 app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const port = 5050
 
