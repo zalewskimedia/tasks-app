14da9b525cb64509f28c51c31e495794cd39d232
Wojciech Zalewski <zalewski.media@gmail.com>
2023-10-27 21:10:19 +0200

    Added middlewares

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const app = express()
 const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
 require("dotenv").config()
+const notFound = require("./middleware/not-found")
 
+app.use(express.static("/public"))
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Jedziemy")
-})
-
 app.use("/api/v1/tasks", tasks)
+app.use(notFound)
 
 const port = 5050
 
